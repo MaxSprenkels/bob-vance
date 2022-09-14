6d5e2075724e23efec92eebc479ca5d91207955a
Max Sprenkels <max@sprenkels.net>
2022-09-14 08:52:20 +0200

    waarom bob

---
+++ why.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Why</title>
+</head>
+<body>
+    <?php include 'navbar.html'; ?>
+    <!-- component -->
+<div class="antialiased w-full h-full bg-gray-300 text-gray-400 font-inter p-10">
+    <div class="container px-4 mx-auto">
+      <div>
+        <div id="title" class="text-center my-10">
+          <h1 class="font-bold text-4xl text-white">Waarom Bob Vance?</h1>
+          <p class="text-light text-gray-500 text-xl">
+            Hier een paar redenen
+          </p>
+        </div>
+        <div
+          class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-evenly gap-10 pt-10"
+        >
+          <div
+            id="plan"
+            class="rounded-lg text-center overflow-hidden w-full transform hover:shadow-2xl hover:scale-105 transition duration-200 ease-in"
+          >
+            <div id="title" class="w-full py-5 border-b border-gray-800">
+              <h2 class="font-bold text-3xl text-white">Goedkoop</h2>
+              <h3 class="font-normal text-indigo-500 text-xl mt-2">
+              </h3>
+            </div>
+            <div id="content" class="">
+              <div id="icon" class="my-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-12 w-12 mx-auto fill-stroke text-indigo-600"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1"
+                    d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                  />
+                </svg>
+                <p class="text-gray-500 text-sm pt-2">
+                  Perfecte prijs kwaliteit verhouding
+                </p>
+              </div>
+              <div id="contain" class="leading-8 mb-10 text-lg font-light">
+                <ul>
+                  <li>Energie klasse C</li>
+                  <li>Laag geluidsniveau</li>
+                  <li>Flessenrek</li>
+                  <li>Draairichting deur verwisselbaar</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div
+            id="plan"
+            class="rounded-lg text-center overflow-hidden w-full transform hover:shadow-2xl hover:scale-105 transition duration-200 ease-in"
+          >
+            <div id="title" class="w-full py-5 border-b border-gray-800">
+              <h2 class="font-bold text-3xl text-white">Betrouwbaar</h2>
+              <h3 class="font-normal text-indigo-500 text-xl mt-2">
+              </h3>
+            </div>
+            <div id="content" class="">
+              <div id="icon" class="my-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-12 w-12 mx-auto fill-stroke text-indigo-600"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1"
+                    d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+                  />
+                </svg>
+                <p class="text-gray-500 text-sm pt-2">
+                  Vandaag besteld binnen 3 werkdagen in huis
+                </p>
+              </div>
+              <div id="contain" class="leading-8 mb-10 text-lg font-light">
+                <ul>
+                  <li>4,5 ster beoordeling</li>
+                  <li>Webshop Keurmerk</li>
+                  <li></li>
+                  <li>Domain included</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div
+            id="plan"
+            class="rounded-lg text-center overflow-hidden w-full transform hover:shadow-2xl hover:scale-105 transition duration-200 ease-in"
+          >
+            <div id="title" class="w-full py-5 border-b border-gray-800">
+              <h2 class="font-bold text-3xl text-white">Enterprise</h2>
+              <h3 class="font-normal text-indigo-500 text-xl mt-2">
+                $19<sup>,99</sup>/month
+              </h3>
+            </div>
+            <div id="content" class="">
+              <div id="icon" class="my-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-12 w-12 mx-auto fill-stroke text-indigo-600"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1"
+                    d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"
+                  />
+                </svg>
+                <p class="text-gray-500 text-sm pt-2">
+                  Perfect large scale team
+                </p>
+              </div>
+              <div id="contain" class="leading-8 mb-10 text-lg font-light">
+                <ul>
+                  <li>10 hours of support</li>
+                  <li>10GB of storage</li>
+                  <li>100GB bandwith</li>
+                  <li>Domain included</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
